69eabf9476484886452599b1000b8548aa72a6d6
BHDHakim <hakim.bhd@gmail.com>
2022-11-11 17:47:57 +0100

    Add codebase

---
+++ _next/static/6KivFLK-lMh4xe8tJOzSP/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/978-d46df48fee33d71d.js","static/chunks/pages/index-2775ed526783f0ce.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
